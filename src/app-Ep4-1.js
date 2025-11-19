f70bda548fee4a85e6a50761f37710326c9010bf
DevangMhatre <devangmhatre396@gmail.com>
2025-11-19 15:18:17 +0530

    Playing with Routing

---
+++ src/app-Ep4-1.js
@@ -0,0 +1,71 @@
+const express = require("express");
+
+const app = express();
+
+// ? Below code of Advanced Routing Techniques are supported on Express v4 and not on v5
+
+// ! By putting `?` BEFORE anything makes that particular thing OPTIONAL means not required
+// * Executed -> /abc, /ac
+// app.get("/ab?c", (req, res) => {
+//   res.send({ firstName: "Devang", lastName: "Mhatre" });
+// });
+
+// ! By putting `b+` AFTER anything makes that particular thing to be inputed in URL as many times but still will be executed
+// * Executed -> /abc, /abbbbbbbbbc, /abbc --- alot of b's but still it will be executed
+// app.get("/ab+c", (req, res) => {
+//   res.send({ firstName: "Devang", lastName: "Mhatre" });
+// });
+
+// ! By putting `*` BETWEEN anything makes so that if we put any letters or words or anything it will still be executed
+// * Executed -> /abcd, /abDEVANGcd, /abadsasdawdwacd --- anything in between ab and cd & it will still be executed
+// app.get("/ab*cd", (req, res) => {
+//   res.send({ firstName: "Devang", lastName: "Mhatre" });
+// });
+
+const separater = "v5";
+
+// ! Fuck this shit.
+app.get(/^\/ab?c$/, (req, res) => {
+  // * To use v4 type in v5 we need to avoid using "" and add Regex -> /^ $/ and between them v4 routing pattern
+  // ?  ^ --> Start of the URL
+  // ?  $ --> End of the URL
+  // ! if we just use Normal Regexp -> / / then it will even match for something like this -> http://localhost:3000/asda/abc/asda
+  // ! to only match http://localhost:3000/abc or http://localhost:3000/ac we need to use /^ $/
+  res.send({ firstName: "Devang", lastName: "Mhatre" });
+});
+
+// app.get(/ab+c/, (req, res) => {
+//   res.send({ firstName: "Devang", lastName: "Mhatre" });
+// });
+
+// app.get(/ab*cd/, (req, res) => {
+//   res.send({ firstName: "Devang", lastName: "Mhatre" });
+// });
+
+// app.get(/a(bc)?d/, (req, res) => {
+//   // * bc is optional now since we grouped bc together using ()
+//   res.send({ firstName: "Devang", lastName: "Mhatre" });
+// });
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000...");
+});
+
+// ! Exploring HTTP Methods
+// // app.use() match all the http method api call to /test
+// app.use("/test", (req, res) => {
+//   res.send("Hello from the server!");
+// });
+
+// // This only handles get call to /users
+// app.get("/user", (req, res) => {
+//   res.send({ firstName: "Devang", lastName: "Mhatre" });
+// });
+
+// app.post("/user", (req, res) => {
+//   res.send("Data successfully saved to the Database");
+// });
+
+// app.delete("/user", (req, res) => {
+//   res.send("Deleted Successfully");
+// });
--- src/app.js
+++ src/app.js
@@ -2,22 +2,9 @@ const express = require("express");
 
 const app = express();
 
-// app.use() match all the http method api call to /test
-app.use("/test", (req, res) => {
+app.get("/user/:id", (req, res) => {
+  console.log(req.params);
   res.send("Hello from the server!");
-});
-
-// This only handles get call to /users
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Devang", lastName: "Mhatre" });
-});
-
-app.post("/user", (req, res) => {
-  res.send("Data successfully saved to the Database");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Deleted Successfully");
 });
 
 app.listen(3000, () => {

1f6db58ee17e3db1b32befac113aa9170538358e
DevangMhatre <devangmhatre396@gmail.com>
2025-11-19 12:01:00 +0530

    Exploring HTTP Methods

--- src/app.js
+++ src/app.js
@@ -2,8 +2,22 @@ const express = require("express");
 
 const app = express();
 
+// app.use() match all the http method api call to /test
 app.use("/test", (req, res) => {
   res.send("Hello from the server!");
+});
+
+// This only handles get call to /users
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Devang", lastName: "Mhatre" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data successfully saved to the Database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted Successfully");
 });
 
 app.listen(3000, () => {

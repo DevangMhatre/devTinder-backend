2c13b429b9b7407be2dfab279aa91e663ec7ff34
DevangMhatre <devangmhatre396@gmail.com>
2025-11-21 14:15:31 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -2,8 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send("User Data Sent");
+app.get("/getUserData", (req, res) => {
+  try {
+    throw new Error("Dasdasd");
+    res.send("User Data Sent");
+  } catch (err) {
+    res.status(500).send("Some Error");
+  }
+});
+
+// ! This won't work if we put it at top since there will be error while checking from first
+// * This will work if there are some error thrown from above code
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    // Log your err
+    res.status(500).send("Something went wrong!");
+  }
 });
 
 app.listen(3000, () => {

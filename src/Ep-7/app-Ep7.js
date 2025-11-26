c0c674843d0854d96e4797de2dcadf522f7a06df
DevangMhatre <devangmhatre396@gmail.com>
2025-11-26 09:22:20 +0530

    API & Schema Validations

--- src/Ep-7/app-Ep7.js
+++ src/Ep-7/app-Ep7.js
@@ -59,41 +59,19 @@ app.delete("/user", async (req, res) => {
 });
 
 // * Update the User
-app.patch("/user/:userId", async (req, res) => {
+app.patch("/user", async (req, res) => {
   // ! We don't have userId in our Schema, so MongoDB just ignores it
-  // const userId = req.body.userId;
-
-  const userId = req.params?.userId;
+  const userId = req.body.userId;
   const data = req.body;
   try {
-    const ALLOWED_UPDATES = [
-      "userId",
-      "photoURL",
-      "skills",
-      "age",
-      "gender",
-      "about",
-    ];
-    const isUpdatedAllowed = Object.keys(data).every((k) =>
-      ALLOWED_UPDATES.includes(k)
-    );
-    if (!isUpdatedAllowed) {
-      throw new Error("Update not allowed.");
-    }
-
-    if (data?.skills > 10) {
-      throw new Error("Skills cannot be more than 10.");
-    }
-
     // ! Options parameters are cool :D
     const user = await User.findByIdAndUpdate(userId, data, {
       returnDocument: "before",
-      runValidators: true,
     });
     // console.log(user);
     res.send("User Updated Successfully!");
   } catch (err) {
-    res.status(400).send("Something went wrong: " + err.message);
+    res.status(400).send("Something went wrong:" + err.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,61 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+// ! EP-8 Data Sanitization
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+      minLength: 3,
+      maxLength: 50,
+    },
+    emailId: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true, // ! Removes extra spaces
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      // * Doesn't work for existing documents by default
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid.");
+        }
+      },
+    },
+    photoURL: {
+      type: String,
+    },
+    about: {
+      type: String,
+      default: "This is a default about of the user",
+    },
+    skills: {
+      // ! By default, if there is an array of something, mongoDB creates an empty space for it even if it's not listed as required
+      type: [String],
+      validate(val) {
+        if (val.length > 10) throw new Error("Skills cannot be more than 10.");
+      },
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 // * Always start with Capital letter when defininig the model name
 const User = mongoose.model("User", userSchema);

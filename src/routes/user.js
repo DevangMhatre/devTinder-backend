561bdf067c2b73fc569f5cc5119b1d6ae7accc2a
DevangMhatre <devangmhatre396@gmail.com>
2025-12-04 23:36:46 +0530

    User Feed - API

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const userAuth = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 
 const userRouter = express.Router();
+const USER_SAFE_DATA = "firstName lastName photoURL age gender about skills";
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
@@ -35,15 +36,37 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         { toUserId: loggedInUser._id, status: "accepted" },
         { fromUserId: loggedInUser._id, status: "accepted" },
       ],
-    }).populate(
-      "fromUserId",
-      "firstName lastName photoURL age gender about skills"
-    );
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
 
-    const data = connectionRequests.map((row) => row.fromUserId);
+    console.log(connectionRequests);
+
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
     res.status(200).json({ data });
   } catch (err) {
     res.status(400).json({ message: "ERROR: " + err.message });
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+    // .populate("fromUserId", "firstName lastName")
+    // .populate("toUserId", "firstName lastName");
+
+    res.send(connectionRequests);
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
   }
 });
 

1821aba05f6b33be36cb1371d92918798d0c2f10
DevangMhatre <devangmhatre396@gmail.com>
2025-12-27 21:28:41 +0530

    dotenv - patch returns

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-const connectDB = require("./config/database");
+const connectDB = require("./src/config/database");
 
 const app = express();
 const PORT = process.env.PORT;
@@ -18,10 +18,10 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-const authRouter = require("./routes/auth");
-const profileRouter = require("./routes/profile");
-const requestRouter = require("./routes/request");
-const userRouter = require("./routes/user");
+const authRouter = require("./src/routes/auth");
+const profileRouter = require("./src/routes/profile");
+const requestRouter = require("./src/routes/request");
+const userRouter = require("./src/routes/user");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);

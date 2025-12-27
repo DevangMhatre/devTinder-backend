1222de87253828a3160c60a1e2a7bf3a851fdb87
DevangMhatre <devangmhatre396@gmail.com>
2025-12-27 21:27:28 +0530

    dotenv - the final patch

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ require("dotenv").config({ path: "ENV_FILENAME" });
 const mongoose = require("mongoose");
 
 // Connects to Cluster
-// * mongoose.connect() returns a promise so we do to async/await
+// mongoose.connect() returns a promise so we do to async/await
 const connectDB = async () => {
   await mongoose.connect(process.env.MONGO_URI);
 };
